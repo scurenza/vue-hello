3e2d1884641cdc570075a1bb114732436438d927
scurenza <fedechiar@hotmail.it>
2022-10-25 15:33:11 +0200

    esercizio e bonus completi

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
+// Descrizione:
+    // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+    // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
 const { createApp } = Vue
 
+createApp({
+    data () {
+        return {
+           message : `Buy `,
+           image : "img/1.png", 
+           alt : "Bitcoin Logo"
+        }
+    }
+}).mount('#app')
